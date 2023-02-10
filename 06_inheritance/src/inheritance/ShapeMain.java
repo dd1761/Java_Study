e754a1aa6fde6cf37004619321de816ff255661e
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-10 15:08:18 +0900

    23-02-10

--- 06_inheritance/src/inheritance/ShapeMain.java
+++ 06_inheritance/src/inheritance/ShapeMain.java
@@ -1,5 +1,34 @@
 package inheritance;
 
+import java.util.Scanner;
+
+class Shape {
+	protected double area;
+	protected Scanner scan = new Scanner(System.in);
+	
+	public Shape() {
+		System.out.println("ShapeTest 기본생성자");
+	}
+	
+	public void calcArea() {
+		System.out.println("ㄷ형을 계산합니다");
+	}
+	
+	public void dispArea() {
+		System.out.println("도형을 출력합니다.");
+	}
+}
+
+//------------------------------
+
+class Sam extends Shape{
+	
+	
+}
+
+
+
+//------------------------
 public class ShapeMain {
 
 	public static void main(String[] args) {
