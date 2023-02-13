4663d83aed5f5d6c09e7001c279288aac9d415a7
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-13 12:18:02 +0900

    23-02-13

--- 06_inheritance/src/inheritance/ObjectMain.java
+++ 06_inheritance/src/inheritance/ObjectMain.java
@@ -1,10 +1,33 @@
 package inheritance;
 
+
+class Test extends Object {
+	
+}
+
+//-------------
+
+class Sample{
+	@Override
+	public String toString() {
+		return "개바부";
+	}
+}
+
+
+//-------------
+
 public class ObjectMain {
 
 	public static void main(String[] args) {
+		Test t = new Test();
+		System.out.println("객체 t = " + t);	//클래스명@16진수 객체 t = inheritance.Test@6f2b958e
+		System.out.println("객체 t = " + t.toString());	//객체 t = inheritance.Test@6f2b958e
+		System.out.println("객체 t = " + t.hashCode());	// main에서 찾다가 없으면 Test로 가서 Object에서 찾는다. 객체 t = 1865127310
+		System.out.println();
 		
-
+		Sample s = new Sample();
+		System.out.println("객체 t = " + s.toString());	//객체 t = 개바부
 	}
 
 }
