14454d6acf86e3ce690c8e2ff7c4c65e901f96c9
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-13 11:29:24 +0900

    23-02-13

--- 05_class/src/class__/StringMain2.java
+++ 05_class/src/class__/StringMain2.java
@@ -8,40 +8,37 @@ public class StringMain2 {
 
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.print("문자열 입력 : ");
+		String original = scanner.next();
 
-			System.out.print("문자열 입력 : ");
-			String original = scanner.next();
+		System.out.print("현재 문자열 입력 : ");
+		String current = scanner.next();
 
-			System.out.print("현재 문자열 입력 : ");
-			String current = scanner.next();
+		System.out.print("바꿀 문자열 입력 : ");
+		String after = scanner.next();
 
-			System.out.print("바꿀 문자열 입력 : ");
-			String after = scanner.next();
-
-			if (original.length() < current.length()) {
-				System.out.println("입력한 문자열의 크기가 작습니다.");
-				System.out.println("치환 할 수 없습니다.");
-				// return; // 함수를 벗어나라
-				System.exit(0); // 프로그램 강제 종료
-			}
-
-			original = original.toLowerCase();
-			current = current.toLowerCase();
-
-			int index = 0;
-			int count = 0;
-			while ((index = original.indexOf("aa", index)) != -1) {
-				count++;
-				index = index + "aa".length();
-			} // while
-//			index = original.indexOf("aa", 0);
-
-			System.out.println(original.replace("aa", after));
-			System.out.println(count + "번 치환");
-			
+		if (original.length() < current.length()) {
+			System.out.println("입력한 문자열의 크기가 작습니다.");
+			System.out.println("치환 할 수 없습니다.");
+			// return; // 함수를 벗어나라
+			System.exit(0); // 프로그램 강제 종료
 		}
 
-	
+		original = original.toLowerCase();
+		current = current.toLowerCase();
+
+		int index = 0;
+		int count = 0;
+		while ((index = original.indexOf(current, index)) != -1) {
+			count++;
+			index = index + current.length();
+		} // while
+//			index = original.indexOf("aa", 0);
+
+		System.out.println(original.replace(current, after));
+		System.out.println(count + "번 치환");
+
+	}
 
 }
 
