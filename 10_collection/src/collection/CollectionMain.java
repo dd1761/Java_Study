3f7c88601dd35fd3a2e5c2d0c868b01ed566ba7c
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-15 12:21:37 +0900

    23-02-15

--- 10_collection/src/collection/CollectionMain.java
+++ 10_collection/src/collection/CollectionMain.java
@@ -6,11 +6,12 @@ import java.util.Iterator;
 
 public class CollectionMain {
 
+	@SuppressWarnings("all")
 	public static void main(String[] args) {
 		Collection coll = new ArrayList();
 		coll.add("호랑이");
 		coll.add("사자");
-		coll.add("호랑이");
+		coll.add("호랑이");	//중복 허용, 순서
 		coll.add(25);
 		coll.add(43.8);
 		coll.add("기린");
@@ -18,7 +19,24 @@ public class CollectionMain {
 		
 		Iterator it = coll.iterator();
 		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+			
+		} // while
+//		it.hasNext()	//항목이 있냐? (true) 없으면 (false)
+//	
+//		it.next()	//항목을 꺼내고 다음 항목으로 이동
+		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
