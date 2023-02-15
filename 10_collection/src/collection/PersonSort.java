25e8e06a9b60704efac7db6b4c07f1995dbaae9c
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-15 17:31:41 +0900

    23-02-15

--- 10_collection/src/collection/PersonSort.java
+++ 10_collection/src/collection/PersonSort.java
@@ -1,6 +1,8 @@
 package collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class PersonSort {
 	
@@ -18,6 +20,30 @@ public class PersonSort {
 		System.out.print("정렬 후 = ");
 		for(String data : ar) {
 			System.out.print(data + "  ");
+		}
+		System.out.println("\n");
+		//-------------------------------------------
+		
+		PersonDTO aa = new PersonDTO("홍길동", 25);
+		PersonDTO bb = new PersonDTO("프로도", 40);
+		PersonDTO cc = new PersonDTO("라이언", 35);
+
+		ArrayList<PersonDTO> list = new ArrayList<PersonDTO>();
+		list.add(aa);
+		list.add(bb);
+		list.add(cc);
+		
+		System.out.println("정렬 전");
+		for(PersonDTO personDTO : list) {
+			System.out.println(personDTO);
+		}
+		System.out.println();
+		
+		Collections.sort(list);
+		
+		System.out.println("정렬 후");
+		for(PersonDTO personDTO : list) {
+			System.out.println(personDTO);
 		}
 		System.out.println();
 	}
