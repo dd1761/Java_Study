dd75eb0db12c38c4bda4b9c057fe192fad11905c
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-15 16:20:59 +0900

    23-02-15

--- 08_interface/src/sungJuk/SungJukDelete.java
+++ 08_interface/src/sungJuk/SungJukDelete.java
@@ -1,12 +1,44 @@
 package sungJuk;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SungJukDelete implements SungJuk{
 
 	@Override
 	public void execute(ArrayList<SungJukDTO> arrayList) {
+		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+	
+		System.out.print("번호 입력 : ");
+		int no = scanner.nextInt();
+	
+		int i;
+		int sw = 0;
+
 		
+		for(SungJukDTO sungJukDTO : arrayList) {	//
+			if(sungJukDTO.getNo() == no) {
+				sw = 1;
+
+				System.out.println(sungJukDTO);
+				
+				System.out.println();
+				System.out.print("수정할 이름 입력 : ");
+				String name = scanner.next();
+			
+				sungJukDTO.setName(name);
+
+				sungJukDTO.calc();
+				
+				System.out.println("수정하였습니다.");
+				break;
+			}//if
+
+		}//for
+		if(sw == 0) {
+			System.out.println("없는 번호입니다.");
+		}//if i==3
 		
 	}
 	
--- 08_interface/src/sungJuk/SungJukMain.java
+++ 08_interface/src/sungJuk/SungJukMain.java
@@ -12,7 +12,6 @@ public class SungJukMain {
 }
 
 
-
 /*
 Package   : sungJuk
 
@@ -78,6 +77,13 @@ Class     : SungJukDTO.java - 1인분
 6. SungJukDelete.java
 - 이름을 입력하여 없는 이름이면 "회원의 정보가 없습니다" 출력하시오
 - 똑같은 이름이 있으면 모두 삭제한다
+
+삭제할 이름 입력 : 천사
+회원의 정보가 없습니다
+
+또는
+
+삭제하였습니다.
 
 7. SungJukSort.java
 
