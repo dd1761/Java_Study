e891b3918007a02cb4f3050b74a9eb32001778fc
KIMDONGBEEN <97274814+dd1761@users.noreply.github.com>
2023-02-17 17:21:44 +0900

    23-02-17

--- 12_io/src/member/MemberPhoneSearch.java
+++ 12_io/src/member/MemberPhoneSearch.java
@@ -4,38 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class MemberPhoneSearch implements Member {
 
 	@Override
 	public void execute(List<MemberDTO> list) {
 		System.out.println();
 		Scanner scanner = new Scanner(System.in);
-	
-		System.out.print("번호 입력 : ");
+
+		System.out.print("검색할 번호 입력 : ");
 		String phone = scanner.next();
-	
+
 		int i;
 		int sw = 0;
 
-		
-		for(MemberDTO memberDTO : list) {	//
-			if(memberDTO.getPhone().equals(phone)) {
+		for (MemberDTO memberDTO : list) { //
+			if (memberDTO.getPhone().equals(phone)) {
 				sw = 1;
-				
 				System.out.println("이름 : " + memberDTO.getName());
 				System.out.println("나이 : " + memberDTO.getAge());
 				System.out.println("휴대폰 번호 : " + memberDTO.getPhone());
 				System.out.println("주소 : " + memberDTO.getAddress());
-				
-				break;
-			}//if
 
-		}//for
-		if(sw == 0) {
-			System.out.println("없는 번호입니다.");
-		}//if i==3
-		
+				break;
+			} // if
+
+		} // for
+		if (sw == 0) {
+			System.out.println("검색한 번호는 없는 번호입니다.");
+		} // if i==3
+
 	}
 
 }
